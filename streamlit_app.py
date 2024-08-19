028a9ba4bec8a89710cf88fbf79baa84fddf294e
mironefimov13 <73971589+mironefimov13@users.noreply.github.com>
2024-08-19 12:16:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,39 @@
 import streamlit as st
+# st.title("Streamlit Tutorial App")
+# st.write("This is my new app")
+# button1 = st.button("Click Me")
+# if button1:
+#     st.write("This is some text")
+# like = st.checkbox("Do you like this app?")
+# button2 = st.button("Submit")
+# if button2:
+#     if like:
+#         st.write("Thanks. I like it too")
+#     else:
+#         st.write("I'm sorry. You have bad tastes!")
+st.title("Computer Commands App")
+st.write("""Welcome to the Computer Commands app.
+         This will calculate any different commands.""")
+st.header("AND")
+and1 = st.checkbox(":1 AND")
+and2 = st.checkbox(":2 AND")
+if and1 and and2:
+    st.write(True)
+else:
+    st.write(False)
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.header("OR")
+or1 = st.checkbox(":1 OR")
+or2 = st.checkbox(":2 OR")
+if or1 or or2:
+    st.write(True)
+else:
+    st.write(False)
+
+st.header("XOR")
+xor1 = st.checkbox(":1 XOR")
+xor2 = st.checkbox(":2 XOR")
+if xor1 != xor2:
+    st.write(True)
+else:
+    st.write(False)
